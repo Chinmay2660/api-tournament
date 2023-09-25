45ce70fa706a557b0c671c12b0a569ccf58e4872
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-09-25 14:22:56 +0530

    Add Mongodb

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const scoresRouter = require("./api/routes/scoreRoutes");
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://0.0.0.0:27017/scores", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb://scmuser:ScmUs3r!!d3vT34m@10.144.96.134:27017/scm_db",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 mongoose.connection.on("connected", () => {
   console.log("Connected to MongoDB");
@@ -18,10 +22,7 @@ mongoose.connection.on("connected", () => {
 mongoose.connection.on("error", (err) => {
   console.error("MongoDB connection error:", err);
 });
-
-const scoresRouter = require("./api/routes/scoreRoutes");
 app.use("/api/scores", scoresRouter);
-
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
